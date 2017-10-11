d7d6d2181dec59df073e6fdde95314c0a647df87
Jason Luo <luoyhang003@hotmail.com>
2017-10-11 10:35:00 -0700

    1-part2

--- Project 1/workspace/code/register_reuse.c
+++ Project 1/workspace/code/register_reuse.c
@@ -4,7 +4,7 @@
  *
  * Author: Yuanhang Luo
  * Create Date: Oct 9th, 2017
- * Revise Date: Oct 9th, 2017
+ * Revise Date: Oct 10th, 2017
  *
  */
 
@@ -33,22 +33,28 @@ int main(int argc, char *argv[]) {
                         int N, loop;
 
                         if(argv[2] == NULL) {
-                                for(loop = 0; loop < 1; loop++) {
+                                for(loop = 0; loop < 6; loop++) {
                                         N = pow(2, 6+loop);
-                                        double a[N*N*N], b[N*N*N], c[N*N*N];
+                                        double *a = NULL;
+                                        double *b = NULL;
+                                        double *c = NULL;
+
+                                        a = (double *)calloc(sizeof(double), N*N);
+                                        b = (double *)calloc(sizeof(double), N*N);
+                                        c = (double *)calloc(sizeof(double), N*N);
+
                                         int index;
 
                                         double max, min;
-                                        max = N*N*N*N;
-                                        min = 0.0-N*N*N*N;
+                                        max = N*N*N;
+                                        min = 0.0-N*N*N;
                                         double range = 0.0+(max - min);
                                         double div = RAND_MAX / range;
                                         srand(time(NULL));
 
-                                        for(index = 0; index < N*N*N; index++) {
+                                        for(index = 0; index < N*N; index++) {
                                                 a[index] = min + (rand()/div);
                                                 b[index] = min + (rand()/div);
-                                                c[index] = min + (rand()/div);
                                         }
 
                                         if(DEBUG) {
@@ -84,7 +90,17 @@ int main(int argc, char *argv[]) {
 
                                         printf("The running time of dgemm0 with n=%d is %f s\n", N, running);
 
+                                        free(a);
+                                        free(b);
+                                        free(c);
 
+                                        a = (double *)calloc(sizeof(double), N*N);
+                                        b = (double *)calloc(sizeof(double), N*N);
+                                        c = (double *)calloc(sizeof(double), N*N);
+                                        for(index = 0; index < N*N; index++) {
+                                                a[index] = min + (rand()/div);
+                                                b[index] = min + (rand()/div);
+                                        }
 
                                         if(DEBUG) {
                                                 printf("(dgemm0) The first N elements in calculated matrix C:\n");
@@ -108,7 +124,7 @@ int main(int argc, char *argv[]) {
                                         }
                                         end = clock();
                                         running = (double)(end - begin) / CLOCKS_PER_SEC;
-                                        printf("The running time of dgemm1 with n=%d is %f s\n", N, running);
+                                        printf("The running time of dgemm1 with n=%d is %f s, Gflops is %f \n", N, running, N*N*N*2.0/running/1000000000.0);
 
                                         if(DEBUG) {
                                                 printf("(dgemm1) The first N elements in calculated matrix C:\n");
@@ -120,20 +136,26 @@ int main(int argc, char *argv[]) {
                                 }
                         }else {
                                 N = atoi(argv[2]);
-                                double a[N*N*N], b[N*N*N], c[N*N*N];
+                                double *a = NULL;
+                                double *b = NULL;
+                                double *c = NULL;
+
+                                a = (double *)calloc(sizeof(double), N*N);
+                                b = (double *)calloc(sizeof(double), N*N);
+                                c = (double *)calloc(sizeof(double), N*N);
+
                                 int index;
 
                                 double max, min;
-                                max = N*N*N*N;
-                                min = 0.0-N*N*N*N;
+                                max = N*N*N;
+                                min = 0.0-N*N*N;
                                 double range = 0.0+(max - min);
                                 double div = RAND_MAX / range;
                                 srand(time(NULL));
 
-                                for(index = 0; index < N*N*N; index++) {
+                                for(index = 0; index < N*N; index++) {
                                         a[index] = min + (rand()/div);
                                         b[index] = min + (rand()/div);
-                                        c[index] = min + (rand()/div);
                                 }
 
                                 int i, j, k;
@@ -156,6 +178,18 @@ int main(int argc, char *argv[]) {
                                 running = (double)(end - begin) / CLOCKS_PER_SEC;
                                 printf("The running time of dgemm0 with n=%d is %f s\n", N, running);
 
+                                free(a);
+                                free(b);
+                                free(c);
+
+                                a = (double *)calloc(sizeof(double), N*N);
+                                b = (double *)calloc(sizeof(double), N*N);
+                                c = (double *)calloc(sizeof(double), N*N);
+
+                                for(index = 0; index < N*N; index++) {
+                                        a[index] = min + (rand()/div);
+                                        b[index] = min + (rand()/div);
+                                }
 
                                 begin = clock();
                                 //dgemm1: simple ijk version triple loop algorithm with register reuse
@@ -171,15 +205,73 @@ int main(int argc, char *argv[]) {
                                 end = clock();
 
                                 running = (double)(end - begin) / CLOCKS_PER_SEC;
-                                printf("The running time of dgemm0 with n=%d is %f s\n", N, running);
+                                printf("The running time of dgemm1 with n=%d is %f s\n", N, running);
 
                         }
                 }
-                else if(!strcmp(argv[1], "dgemm1")) {
-                        printf("=========Run dgemm1===============\n");
+                else if(!strcmp(argv[1], "part2")) {
+                        printf("=========PART TWO===============\n");
+
+                        int N, loop;
+
+                        double *a = NULL;
+                        double *b = NULL;
+                        double *c = NULL;
+
+                        for(loop = 0; loop < 6; loop++) {
+                          N = pow(2, loop+6);
+
+                          a = (double*)calloc(sizeof(double), N*N);
+                          b = (double*)calloc(sizeof(double), N*N);
+                          c = (double*)calloc(sizeof(double), N*N);
+
+                          int index;
+
+                          double max, min;
+                          max = N*N*N;
+                          min = 0.0-N*N*N;
+                          double range = 0.0+(max - min);
+                          double div = RAND_MAX / range;
+                          srand(time(NULL));
+
+                          for(index = 0; index < N*N; index++) {
+                                  a[index] = min + (rand()/div);
+                                  b[index] = min + (rand()/div);
+                          }
+
+
+                          //dgemm2: more aggresive register reuse
+                          int i, j, k;
+
+                          clock_t begin;
+                          clock_t end;
+                          double running;
+
+                          begin = clock();
+
+                          for(i = 0; i < N; i += 2) {
+                            for(j = 0; j < N; j += 2) {
+                              for(k = 0; k < N; k += 2) {
+                                c[i*N + j] = a[i*N + k] * b[k*N + j] + a[i*N + k+1] * b[(k+1)*N + j] + c[i*N + j];
+                                c[(i+1)*N + j] = a[(i+1)*N + k] * b[k*N + j] + a[(i+1)*N + k+1] * b[(k+1)*N + j] + c[(i+1)*N + j];
+                                c[i*N + (j+1)] = a[i*N + k] * b[k*N + (j+1)] + a[i*N + k+1] * b[(k+1)*N + (j+1)] + c[i*N + (j+1)];
+                                c[(i+1)*N + (j+1)] = a[(i+1)*N + k] * b[k*N + (j+1)] + a[(i+1)*N + k+1] * b[(k+1)*N + (j+1)] + c[(i+1)*N + (j+1)];
+                              }
+                            }
+
+                          }
+
+                          end = clock();
+                          running = (double)(end - begin) / CLOCKS_PER_SEC;
+                          printf("The running time of dgemm2 with n=%d is %f s, \n", N, running);
+
+
+                        }
+
+
                 }
-                else if(!strcmp(argv[1], "dgemm2")) {
-                        printf("=========Run dgemm2===============\n");
+                else if(!strcmp(argv[1], "part3")) {
+                        printf("=========PART THREE===============\n");
                 }
                 else{
                         printf("Please input the correct parameters or use `./proj1 help` to see instructions.\n");
