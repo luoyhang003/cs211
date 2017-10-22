f50a652234df58fc4ffc5dd111f9acc2c7a2b9d3
Jason Luo <luoyhang003@hotmail.com>
2017-10-22 14:28:08 -0700

    lab2

---
+++ Project2/code/mylu.m
@@ -0,0 +1,58 @@
+function mylu(n)
+A=randn(n,n); 
+b=randn(n,1); 
+Abk=A; 
+pvt = 1:n;
+%Factorize A. Your task: transform this part to mydgetrf(). 
+for i = 1 : n-1
+    % pivoting %
+    maxind=i; max=abs(A(i,i)); 
+    for t=i+1:n
+        if ( abs(A(t,i))>max ) 
+            maxind=t; max=abs(A(t,i));
+        end
+    end
+    if (max==0)
+        disp ( 'LUfactoration failed: coefficient matrix is singular' ); 
+        return;
+    else
+        if (maxind ~= i )
+        %save pivoting infomation 
+            temps=pvt(i); 
+            pvt(i)=pvt(maxind); 
+            pvt(maxind)=temps; 
+            %swap rows
+            tempv=A(i,:); 
+            A(i,:)=A(maxind,:); 
+            A(maxind,:)=tempv;
+        end
+    end
+    %factorization 
+    for j = i+1 : n,
+        A(j,i) = A(j,i)/A(i,i); 
+        for k = i+1 : n,
+            A(j,k) = A(j,k) - A(j,i) * A(i,k); 
+        end
+    end
+end
+
+%verify my factorization with Matlab for small matrix by printing results on screen. May skip in your code 
+myfactorization=A
+mypivoting=pvt
+[Matlab_L, Matlab_U, Matlab_P]=lu(Abk)
+%forward substitution. Your task: transform this part to mydtrsm().
+
+%forward substitution. Your task: transform this part to mydtrsm(). 
+y(1) = b(pvt(1));
+for i = 2 : n,
+    y(i) = b(pvt(i)) - sum ( y(1:i-1) .* A(i, 1:i-1) ); 
+end
+% back substitution. Your task: transform this part to mydtrsm(). 
+x(n) = y(n) / A(n, n);
+for i = n-1 : -1 : 1,
+    x(i) = ( y(i) - sum ( x(i+1:n) .* A(i, i+1:n) ) ) / A(i,i); 
+end
+%Matlab solve. Your task: call dgetrf() to factorize and dtrsm() twice (back and forward substit.) to solve. 
+xx= Abk\b;
+%verify my solution with matlab. Your task: verify your solution with the solution from LAPACK. 
+Solution_Difference_from_Matlab=norm(x'-xx)
---
+++ Project2/code/part1.cpp
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include "stdlib.h"
+#include "math.h"
+#include <time.h>
+#include <string.h>
+//#include "lapacke.h"
+//#include "blas.h"
+
+// using namespace std;
+
+int mydgetrf(double* m, int N, int* pvt);
+int mydtrsm(int N, double* A, int* pvt, double* b, double* x);
+
+int main(int argc, char* argv[]) {
+        if(argc > 1) {
+                int N=0;
+                N = atoi(argv[1]);
+                // printf("N=%d\n", N);
+
+                char TRANS = 'N';
+                int INFO = N;
+                int LDA = N;
+                int LDB = N;
+                // N = 3;
+                int NRHS = 1;
+                int IPIV[N];
+
+                double* B = (double*)calloc(sizeof(double), N);
+
+                for(int i = 0; i < N; i++) {
+                  B[i] = 1;
+                }
+
+                char SIDE = 'L';
+                char UPLO = 'L';
+                char DIAG = 'U';
+                int M    = 1;
+                double a    = 1.0;
+
+                double *A = NULL;
+                A = (double*)calloc(sizeof(double), N*N);
+
+                double *A1 = NULL;
+                A1 = (double*)calloc(sizeof(double), N*N);
+
+                double *b = NULL;
+                b = (double*)calloc(sizeof(double), N);
+
+                double *x = NULL;
+                x = (double*)calloc(sizeof(double), N);
+
+                double max, min;
+                max = N;
+                min = 0.0;
+                double range = 0.0+(max - min);
+                double div = RAND_MAX / range;
+                srand(time(NULL));
+
+                for(int index = 0; index < N*N; index++) {
+                        A[index] = min + (rand()/div);
+                        A1[index] = A[index];
+                }
+
+                for(int index = 0; index < N; index++) {
+                  b[index] = min + (rand()/div);
+                }
+
+
+
+                int *pvt = new int[N];
+
+                for(int i = 0; i < N; i++) {
+                        pvt[i] = i;
+                }
+
+                // for(int i = 0; i < N; i++) {
+                //         for(int j = 0; j < N; j++) {
+                //                 printf("%f ",A[i*N+j]);
+                //         }
+                //         printf("\n");
+                // }
+
+                clock_t start, end;
+                double running;
+
+                start=clock();
+
+                int n = mydgetrf(A, 3, pvt);
+                n = mydtrsm(N, A, pvt, b, x);
+
+                end = clock();
+
+                running = (double)(end - start)/1000000.0;
+
+                printf("MY Running: %f\n", running);
+                // printf("Error: %d\n", n);
+
+                // for(int i = 0; i < N; i++) {
+                //         for(int j = 0; j < N; j++) {
+                //                 printf("%f ",A[i*N+j]);
+                //         }
+                //         printf("\n");
+                // }
+                //
+                // for(int j = 0; j < N; j++) {
+                //         printf("%f ",pvt[j]);
+                // }
+
+                start=clock();
+
+                // LU factorization
+                LAPACK_dgetrf(&N,&N,A1,&LDA,IPIV,&INFO);
+
+                for(int i = 0; i < N; i++)
+                {
+                        double tmp = B[IPIV[i]-1];
+                        B[IPIV[i]-1] = B[i];
+                        B[i] = tmp;
+                }
+
+                // forward  L(Ux) = B => y = Ux
+                dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,A1, &N, B, &N);
+                UPLO = 'U';
+                DIAG = 'N';
+                // backward Ux = y
+                dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,A1, &N, B, &N);
+
+                end = clock();
+
+                running = (double)(end - start)/1000000.0;
+
+                printf("LAPACK Running: %f\n", running);
+
+
+        }else {
+                printf("Please input the size of the MATRIX.\n");
+        }
+}
+
+int mydgetrf(double* m, int N, int *pvt) {
+        double max;
+        int maxind;
+        for(int i = 0; i < N-1; i++) {
+                maxind = i;
+                max = fabs(m[i*N+i]);
+                for(int t = i+1; t < N; t++) {
+                        if(fabs(m[t*N+i]) > max) {
+                                maxind = t;
+                                max = fabs(m[t*N+i]);
+                        }
+                }
+                if(max == 0) {
+                        printf("LUfactoration failed: coefficient matrix is singular.\n");
+                        return -1;
+                }else {
+                        if(maxind != i) {
+                                //save pivoting infomation
+                                double temps = pvt[i];
+                                pvt[i] = pvt[maxind];
+                                pvt[maxind] = temps;
+                                //swap rows
+                                for(int j = 0; j < N; j++) {
+                                        double tempv = m[i*N+j];
+                                        m[i*N+j] = m[maxind*N+j];
+                                        m[maxind*N+j] = tempv;
+                                }
+                        }
+                        //factorization
+                        for(int j = i+1; j < N; j++) {
+                                m[j*N+i] = m[j*N+i]/m[i*N+i];
+                                for(int k = i+1; k < N; k++) {
+                                        m[j*N+k] = m[j*N+k] - m[j*N+i]*m[i*N+k];
+                                }
+                        }
+                }
+
+
+
+
+
+        }
+
+        // for(int j = 0; j < N; j++) {
+        //         printf("%f ",pvt[j]);
+        // }
+
+
+        // for(int i = 0; i < N; i++) {
+        //         for(int j = 0; j < N; j++) {
+        //                 printf("%f ", m[i*N+j]);
+        //         }
+        //         printf("\n");
+        // }
+
+        return 0;
+
+}
+
+int mydtrsm(int N, double* A, int* pvt, double* b, double* x) {
+  //forward substitution.
+  double* y = (double*)calloc(sizeof(double), N);
+  y[1] = b[pvt[1]];
+  for(int i = 1; i < N; i++) {
+    double sum = 0.0;
+    for(int j = 0; j < i-1; j++) {
+      sum += y[j] * A[j*N+j];
+    }
+    y[i] = b[pvt[i]] - sum;
+  }
+  //back substitution.
+  x[N-1] = y[N-1]/A[N*N-1];
+  for(int i = N-2; i >= 0; i--) {
+    double sum = 0.0;
+    for(int j = i+1; j < N; j++) {
+      sum += x[j]*A[i*N+j];
+    }
+    x[i] = (y[i] - sum)/A[i*N+i];
+  }
+
+  return 0;
+
+}
