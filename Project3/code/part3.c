b76a71cf51046c95bbad69674f97fd5e53b04a66
Yuanhang Luo <luoyhang003@Hotmail.com>
2017-11-17 16:11:31 -0800

    Update part3.c

--- Project3/code/part3.c
+++ Project3/code/part3.c
@@ -7,12 +7,6 @@
 
 #define MIN(a,b)  ((a)<(b)?(a):(b))
 
-// Defining Block Low, Block High and Block Size
-
-//#define BLOCK_LOW(id, p, n) ((id)*(n)/(p))
-//#define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id)+1, p,n)-1)
-//#define BLOCK_SIZE(id, p, n) (BLOCK_LOW((id)+1,p,n)-BLOCK_LOW((id),p,n))
-//#define BLOCK_OWNER(index,p,n) ((((p) * index)+1)-1)/(n)
 
 int main(int argc, char *argv[])
 {
